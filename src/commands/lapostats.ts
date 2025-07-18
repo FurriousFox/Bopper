b53defcb03ec8395fe54073f4aa5787f46f3037f
Micha <micha@argv.nl>
2025-07-18 17:23:24 +0200

    add lapo notice

--- src/commands/lapostats.ts
+++ src/commands/lapostats.ts
@@ -6,13 +6,16 @@ export default {
     examples: [],
     description: 'show leaderboard for lapo points',
     async handler(message: Message): Promise<void> {
+        const prefix = database.read({
+            guildId: message.guildId!,
+            property: "prefix",
+        }) ?? ".";
         const members = await message.guild!.members.fetch();
-
         const lapos = database.readAll({
             like: `${message.guildId}A`,
             property: "lapos",
         });
-        const leaderboard = `## Lapo stats\n${lapos.sort((a, b) => (+b.value) - (+a.value)).map(e => [e.key.split("--")[1], e.value]).filter(e => members.get(e[0])?.user?.bot === false).map(e => `<@${e[0]}>: ${e[1]}`).join("\n")}\n\n-# you gain 1 lapo point when being the last user of the day to use the lapo command`;
+        const leaderboard = `## Lapo stats\n${lapos.sort((a, b) => (+b.value) - (+a.value)).map(e => [e.key.split("--")[1], e.value]).filter(e => members.get(e[0])?.user?.bot === false).map(e => `<@${e[0]}>: ${e[1]}`).join("\n")}\n\n-# you gain 1 lapo point when being the last user of the day to use the lapo command\n-# determined by when the message is received by discord (see ${prefix}ping for more info)`;
 
         message.reply({ content: leaderboard, allowedMentions: {} });
     }
