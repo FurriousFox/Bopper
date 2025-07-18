d5bc13b460277e968d034d81cfbfcd15743d50d9
Micha <micha@argv.nl>
2025-07-18 18:03:28 +0200

    fix ping on edit

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -10,10 +10,11 @@ export default {
         let response = '';
         const nonce = message.nonce ? SnowflakeUtil.timestampFrom(message.nonce.toString()) : undefined;
         const id = SnowflakeUtil.timestampFrom(message.id);
+        const id_edit = message.editedTimestamp ?? SnowflakeUtil.timestampFrom(message.id);
         const now = +new Date();
 
         if (nonce) response += `latency:   ${Math.round((id - nonce))}ms`;
-        response += `\nping: ${nonce ? "        " : ""}${Math.round((now - id))}ms\n`;
+        response += `\nping: ${nonce ? "        " : ""}${Math.round((now - id_edit))}ms\n`;
 
         if (nonce) response += "\n-# latency: time between you sending your message, and the message being received by discord";
         response += "\n-# ping: time between discord receiving your message, and the message being received by the bot";
