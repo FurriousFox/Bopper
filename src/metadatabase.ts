be0f0eaa17778e2b787ebbc83e9d36acb3572681
Micha <micha@argv.nl>
2025-11-16 23:56:58 +0100

    add database purely for metadata

---
+++ src/metadatabase.ts
@@ -0,0 +1,101 @@
+import { Snowflake } from 'discord.js';
+import { DatabaseSync } from "node:sqlite";
+import path from "node:path";
+
+const db = new DatabaseSync(path.join(import.meta.dirname ?? "", "../bopper_meta.db"));
+
+export type readFunction = (options: {
+    guildId: Snowflake;
+    channelId?: Snowflake;
+    userId?: Snowflake;
+    messageId?: Snowflake;
+
+    property: string;
+}) => string | undefined;
+export type readAllFunction = (options: {
+    like: string;
+
+    property: string;
+}) => { key: string, value: string; }[];
+
+export type writeFunction = (options: {
+    guildId: Snowflake;
+    channelId?: Snowflake;
+    userId?: Snowflake;
+    messageId?: Snowflake;
+
+    property: string;
+    value: string | number;
+}) => boolean;
+
+export type removeFunction = (options: {
+    guildId: Snowflake;
+    channelId?: Snowflake;
+    userId?: Snowflake;
+    messageId?: Snowflake;
+
+    property: string;
+} | {
+    key: string;
+
+    property: string;
+}) => boolean;
+
+
+export const read: readFunction = function (options) {
+    if (!options.property.match(/^[a-zA-Z_]\w+$/)) return undefined;
+    db.exec(`CREATE TABLE IF NOT EXISTS ${options.property} ( key TEXT PRIMARY KEY, value TEXT )`);
+
+    const query = db.prepare(`SELECT value FROM ${options.property} WHERE key=?`);
+    const result = query.get(`${options.guildId}A${options.channelId ?? ""}--${options.userId ?? ""}${options.messageId ? `C${options.messageId}` : ""}`);
+    if (result == undefined) return undefined;
+    if (result.value == undefined) return undefined;
+    return result.value.toString();
+};
+
+export const readAll: readAllFunction = function (options) {
+    if (!options.property.match(/^[a-zA-Z_]\w+$/)) return [];
+    db.exec(`CREATE TABLE IF NOT EXISTS ${options.property} ( key TEXT PRIMARY KEY, value TEXT )`);
+
+    const query = db.prepare(`SELECT * FROM ${options.property} WHERE key LIKE ?`);
+    const result = query.all(`%${options.like}%`);
+    if (result == undefined) return [];
+    return result as { key: string, value: string; }[];
+};
+
+export const write: writeFunction = function (options) {
+    if (!options.property.match(/^[a-zA-Z_]\w+$/)) return false;
+    db.exec(`CREATE TABLE IF NOT EXISTS ${options.property} ( key TEXT PRIMARY KEY, value TEXT )`);
+
+    db.prepare(`INSERT OR REPLACE INTO ${options.property} (key, value) VALUES (?,?)`)
+        .run(`${options.guildId}A${options.channelId ?? ""}--${options.userId ?? ""}${options.messageId ? `C${options.messageId}` : ""}`, options.value.toString());
+
+    return true;
+};
+
+export const remove: removeFunction = function (options) {
+    if (!options.property.match(/^[a-zA-Z_]\w+$/)) return false;
+    db.exec(`CREATE TABLE IF NOT EXISTS ${options.property} ( key TEXT PRIMARY KEY, value TEXT )`);
+
+    if ("key" in options) {
+        db.prepare(`DELETE FROM ${options.property} WHERE key=?`)
+            .run(options.key);
+    } else {
+        db.prepare(`DELETE FROM ${options.property} WHERE key=?`)
+            .run(`${options.guildId}A${options.channelId ?? ""}--${options.userId ?? ""}${options.messageId ? `C${options.messageId}` : ""}`);
+    }
+
+    return true;
+};
+
+
+
+declare global {
+    var metabase: {
+        read: readFunction;
+        readAll: readAllFunction;
+        write: writeFunction;
+        remove: removeFunction;
+    };
+}
+globalThis.metabase = { read, write, readAll, remove };
