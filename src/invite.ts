61a2f814c5dc950c1dad7d0557692870a46c9f01
Micha <micha@argv.nl>
2025-11-23 17:12:46 +0100

    use client user id as fallback

--- src/invite.ts
+++ src/invite.ts
@@ -5,10 +5,10 @@ const interactions: Record<Snowflake, ChatInputCommandInteraction | Message> = {
 export function invite_ephemeral(interaction: ChatInputCommandInteraction | Message, invite = false, ephemeral = true) {
     const reply = interaction.reply({
         content: (invite ? `Add Bopper to get access to all of its features!\n\n` :
-            `Bopper must be added to this server to use this command\n\n`) + `-# https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? "1395103292899590337"}`,
+            `Bopper must be added to this server to use this command\n\n`) + `-# https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? interaction.client.user.id ?? "1395103292899590337"}`,
         allowedMentions: {},
-        ...((interaction instanceof ChatInputCommandInteraction && ephemeral) ? { flags: 0 | (ephemeral ? MessageFlags.Ephemeral : 0), components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link), new ButtonBuilder().setLabel("Send visibly").setCustomId("public_add").setStyle(ButtonStyle.Secondary)).toJSON()], } : {
-            components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link)).toJSON()]
+        ...((interaction instanceof ChatInputCommandInteraction && ephemeral) ? { flags: 0 | (ephemeral ? MessageFlags.Ephemeral : 0), components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? interaction.client.user.id ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link), new ButtonBuilder().setLabel("Send visibly").setCustomId("public_add").setStyle(ButtonStyle.Secondary)).toJSON()], } : {
+            components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? interaction.client.user.id ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link)).toJSON()]
         })
     });
 
@@ -18,8 +18,8 @@ export function invite_ephemeral(interaction: ChatInputCommandInteraction | Mess
 
 export function invite(interaction: ButtonInteraction | Message, invite = false) {
     const reply = {
-        content: (invite ? `Add Bopper to get access to all of its features!\n\n` : `Bopper must be added to this server for some features to work\n\n`) + `-# invite link: https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? "1395103292899590337"}\n-# source code: <https://github.com/FurriousFox/Bopper>`,
-        components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link)).toJSON()],
+        content: (invite ? `Add Bopper to get access to all of its features!\n\n` : `Bopper must be added to this server for some features to work\n\n`) + `-# invite link: https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? interaction.client.user.id ?? "1395103292899590337"}\n-# source code: <https://github.com/FurriousFox/Bopper>`,
+        components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("Add Bopper").setURL(`https://discord.com/oauth2/authorize?client_id=${interaction.applicationId ?? interaction.client.user.id ?? "1395103292899590337"}`).setStyle(ButtonStyle.Link)).toJSON()],
         allowedMentions: {},
     };
 
